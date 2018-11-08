03cb862e9e124512f9fdaa0d45cd78021e156669
Emerald <epodbicani1@etf.unsa.ba>
2018-11-08 19:49:38 +0100

    dodana funkcija validnostpozicije (SAMO U ABSTRACT)

--- src/ba/unsa/etf/rpr/ChessPiece.java
+++ src/ba/unsa/etf/rpr/ChessPiece.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr;
-
+import java.util.Arrays;
+import java.util.List;
 public abstract class ChessPiece {
 
         String position;
@@ -18,9 +19,25 @@ public abstract class ChessPiece {
         public abstract void move(String position);
 
             ChessPiece(String pozicija, Color boja){
+                if (!ValidnostPozicije(position)) throw new IllegalArgumentException();
             this.position=pozicija;
             this.color=boja;
         }
-
+    public boolean ValidnostPozicije(String position){
+        if (position.length() != 2) {
+            return false;
+        }
+        char prvaoznaka = position.charAt(0);
+        List<Character> prvihoznakalista = Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h');
+        if (!prvihoznakalista.contains(prvaoznaka)){
+            return false;
+        }
+        char drugaoznaka = position.charAt(1);
+        List<Character> drugihoznakalista = Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8');
+        if (!drugihoznakalista.contains(drugaoznaka)){
+            return false;
+        }
+        return true;
+    }
 
 }
--- src/ba/unsa/etf/rpr/Pawn.java
+++ src/ba/unsa/etf/rpr/Pawn.java
@@ -24,20 +24,5 @@ public class Pawn extends ChessPiece {
         if (!ValidnostPozicije(position)) throw new IllegalArgumentException();
     }
 
-    public boolean ValidnostPozicije(String position){
-        if (position.length() != 2) {
-            return false;
-        }
-        char prvaoznaka = position.charAt(0);
-        List<Character> prvihoznakalista = Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h');
-        if (!prvihoznakalista.contains(prvaoznaka)){
-            return false;
-        }
-        char drugaoznaka = position.charAt(1);
-        List<Character> drugihoznakalista = Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8');
-        if (!drugihoznakalista.contains(drugaoznaka)){
-            return false;
-        }
-        return true;
-    }
+    
 }
