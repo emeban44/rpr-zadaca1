ad60b0a7216c91ef1a1b8671b02ff6d3dd935d53
Emerald <epodbicani1@etf.unsa.ba>
2018-11-08 18:40:30 +0100

    klasa ChessPiece

---
+++ src/ba/unsa/etf/rpr/ChessPiece.java
@@ -0,0 +1,26 @@
+package ba.unsa.etf.rpr;
+
+public abstract class ChessPiece {
+
+        String position;
+
+        public enum Color{
+            WHITE,
+            BLACK
+        }
+
+        Color color;
+
+        public abstract String getPosition();
+
+        public abstract Color getColor();
+
+        public abstract void move(String position);
+
+        ChessPiece(String pozicija, Color boja){
+            this.position=pozicija;
+            this.color=boja;
+        }
+
+
+}
